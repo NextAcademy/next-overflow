a543ecba5cb62d7c66b3c14be2605dcea1f513fb
cySheng <cyusheng93@gmail.com>
2017-04-27 10:23:16 +0800

    Ran migration file for questions

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170427020635) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "questions", force: :cascade do |t|
     t.string   "content"
     t.datetime "created_at", null: false

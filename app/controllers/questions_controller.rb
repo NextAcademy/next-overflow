005d72fddce619722338311b5e7ac09a21d1a2ee
cySheng <cyusheng93@gmail.com>
2017-04-27 10:38:31 +0800

    Added a title column for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
 	def index
-		
+		@question = Question.new
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20170427020635) do
   enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
+    t.string   "title"
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

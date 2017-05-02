6fe0b1bdb5a8e6cc4ef122404609b6a8fa7a4b41
cySheng <cyusheng93@gmail.com>
2017-05-02 11:38:10 +0800

    Fixed several bugs regarding enum role

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
 	def index
+		byebug
 		@question = Question.new
 		@questions = Question.all 
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   include Clearance::User
   validates :name, presence: true
   has_many :questions
-  enum status: [ :student, :mentor ]
+  enum role: [ :student, :mentor ]
 end

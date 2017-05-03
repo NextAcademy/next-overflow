1f1d350a4cac98dee68692d2a435748ed4a4d427
cySheng <cyusheng93@gmail.com>
2017-05-03 12:56:14 +0800

    Added a superadmin role for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   include Clearance::User
   validates :name, presence: true
   has_many :questions
-  enum role: [ :student, :mentor ]
+  enum role: [ :student, :mentor, :superadmin ]
 end

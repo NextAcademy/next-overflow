3a24660eea381b7029b83a8d465fcb213585f62a
cySheng <cyusheng93@gmail.com>
2017-05-04 16:00:29 +0800

    Generated a model for admin users and specified its column types and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   include Clearance::User
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
+  
   has_many :questions
 
   enum role: [:student, :mentor, :superadmin]
 
-  
 end

d635233044d335328ab0d71f7f6b268e94b5c786
cySheng <cyusheng93@gmail.com>
2017-05-04 16:56:04 +0800

    Configured authorization adapter to be pundit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502031536) do
+ActiveRecord::Schema.define(version: 20170504075005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "name",                           null: false
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128, null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128, null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
